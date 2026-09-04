0eeeb359772c2ea1bc652cc57bdb3788e87cb8c9
Cenk Kılıç <cenk1cenk2@gmail.com>
2026-09-04 20:35:34 +0200

    feat: move print screen under the layer lock

--- keyboards/sofle/keymaps/cenk1cenk2/keymap.c
+++ keyboards/sofle/keymaps/cenk1cenk2/keymap.c
@@ -20,8 +20,8 @@ KC_TRNS,            KC_LCBR,              KC_RCBR,              KC_UNDS,        
 [_RAISE]  = LAYOUT(
 KC_TRNS,            KC_NO,                KC_NO,                KC_NO,              KC_NO,               KC_NO,                                                         KC_NO,               KC_NO,              KC_NO,                KC_NO,              KC_NO,               KC_NO,
 KC_TRNS,            KC_F1,                KC_F2,                KC_F3,              KC_F4,               KC_NO,                                                         QK_LLCK,             LCTL(KC_LEFT),      KC_UP,                LCTL(KC_RIGHT),     KC_PGUP,             KC_DEL,
-KC_TRNS,            KC_F5,                KC_F6,                KC_F7,              KC_F8,               KC_NO,                                                         KC_NO,               KC_LEFT,            KC_DOWN,              KC_RGHT,            KC_PGDN,             KC_ENT,
-KC_TRNS,            KC_F9,                KC_F10,               KC_F11,             KC_F12,              KC_NO,               KC_TRNS,             KC_TRNS,             KC_PSCR,             KC_HOME,            KC_NO,                KC_END,             KC_NO,               KC_NO,
+KC_TRNS,            KC_F5,                KC_F6,                KC_F7,              KC_F8,               KC_NO,                                                         KC_PSCR,             KC_LEFT,            KC_DOWN,              KC_RGHT,            KC_PGDN,             KC_ENT,
+KC_TRNS,            KC_F9,                KC_F10,               KC_F11,             KC_F12,              KC_NO,               KC_TRNS,             KC_TRNS,             KC_NO,               KC_HOME,            KC_NO,                KC_END,             KC_NO,               KC_NO,
                     KC_TRNS,              KC_TRNS,              KC_TRNS,            KC_TRNS,             KC_TRNS,                                                       KC_TRNS,             KC_TRNS,            KC_TRNS,              KC_TRNS,            KC_TRNS
 ),
 [_ADJUST] = LAYOUT(
